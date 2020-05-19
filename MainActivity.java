d109a52df6f2f821f47e9f5f688d7f09b4f78164
rlamj5555 <rlamj5555@naver.com>
2020-05-19 22:58:19 +0900

    20-5-19-JAVA_PRACTICE

---
+++ MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.everything_s_ok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ Practice_0519.java
@@ -0,0 +1,20 @@
+package com.example.everything_s_ok;
+import java.util.ArrayList;
+
+public class Practice_0519 {
+    /*
+    3  1  3
+    2  3  2(2*n) -1
+    1  5  1
+    0  7  0
+    */
+    public static void main(String[] args){
+        ArrayList<Integer> frst = new ArrayList<>();
+        for(int i=0; i<7;i++){
+            int result = (2*i)-1;
+            if(result<2){
+               System.out.print(" ");
+        } System.out.print("*");
+        }
+    }
+}
