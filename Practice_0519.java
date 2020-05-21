7bc7dacadbf01bb97e8fd194d2a6a1a8775e2c77
rlamj5555 <rlamj5555@naver.com>
2020-05-21 21:14:57 +0900

    20-5-21-JAVA_PRACTICE

--- Practice_0519.java
+++ Practice_0519.java
@@ -8,13 +8,45 @@ public class Practice_0519 {
     1  5  1
     0  7  0
     */
-    public static void main(String[] args){
-        ArrayList<Integer> frst = new ArrayList<>();
-        for(int i=0; i<7;i++){
-            int result = (2*i)-1;
-            if(result<2){
-               System.out.print(" ");
-        } System.out.print("*");
+
+    public static void main(String[] args) {
+
+
+        for(int a=0;a<7;a++){
+            System.out.print("*"); }
+        System.out.println();
+        for(int a=0; a<4; a++){
+            for(int i=0; i<1;i++){
+                System.out.print(" ");
+            }
+            if(a==0){
+                for(int i=0; i<5; i++){
+                    System.out.print("*");
+                }
+            }
+        }
+        System.out.println();
+        for (int a=0; a<4;a++){
+            for(int i=0;i<2;i++){
+                System.out.print(" ");
+            }
+            if(a==0) {
+                for (int i = 0; i < 3; i++) {
+                    System.out.print("*");
+                }
+            }else{System.out.print(" ");}
+
+        }
+        System.out.println();
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 3; j++) {
+                if (j == 0) {
+                    System.out.print("*");
+                }else{System.out.print(" ");}
+            }return;
         }
     }
 }
